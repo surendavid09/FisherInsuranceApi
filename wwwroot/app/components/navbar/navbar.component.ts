0ee727ff65dbd66d06ef44709e8d6e26fea6017b
Suren David <surendavid09@gmail.com>
2017-04-04 20:12:50 -0400

    commit

---
+++ wwwroot/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'nav-bar',
+    templateUrl: './app/component/navbar/navbar.component.html',
+    styleUrls: ['./app/components/navbar/navbar.component.css']
+})
+
+export class NavBarComponent {
+    
+}
