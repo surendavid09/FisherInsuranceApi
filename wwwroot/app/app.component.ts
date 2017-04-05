bf36c0f1fc1540026ff7f9aa73e478d4b126db18
Suren David <surendavid09@gmail.com>
2017-04-04 21:56:57 -0400

    commit

--- wwwroot/app/app.component.ts
+++ wwwroot/app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: 'fisher-insurance',
-    templateUrl: './app/app.component.html'
+    templateUrl: './app.component.html'
 })
 export class AppComponent { }
